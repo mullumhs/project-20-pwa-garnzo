aa0591d46d936d3c25ab18fa8b2b59cc0d53195d
Garnzo <92022699+garnzo@users.noreply.github.com>
2024-11-20 10:39:12 +1100

    finished database model today, started on index.html and added bootstrap.

--- models.py
+++ models.py
@@ -4,3 +4,14 @@ db = SQLAlchemy()
 
 # Define your database model here
 # Example: class Item(db.Model):
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.Text, nullable=True)
+    publisher = db.Column(db.Date, nullable=True)
+    year = db.Column(db.Integer, nullable=True)
+    rating = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
