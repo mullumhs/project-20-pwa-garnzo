a95a382068d93037d916e3a3113825a735a7ec8b
Garnzo <92022699+garnzo@users.noreply.github.com>
2025-09-10 14:00:51 +1000

    I finished my project, creating the views for edit task, add taski global day tracker, and assigned assortment of tasks alongside the db for affirmations and the completion of them, tasks get sorted by yesterdays tasks and todays tasks and displayed on todo.html as yesterday + day value and today + day value, i didnt end up doing a delete

--- models.py
+++ models.py
@@ -8,9 +8,15 @@ db = SQLAlchemy()
 class todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    type = db.Column(db.String(100), nullable=False)
-    day = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    day = db.Column(db.String(10), nullable=False)
     value = db.Column(db.Integer, nullable=False)
+    completed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f'<ToDo {self.name}>'
+    
+class Affirmation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    day = db.Column(db.String(10), nullable=False)
--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect, url_for, flash
-from models import db, todo # Also import your database model here
+from models import db, todo, Affirmation
+from datetime import date, timedelta
 types = [
              "Self Maintainence",
              "Work",
@@ -20,58 +21,161 @@ def init_routes(app):
         tasks = todo.query.all()
         return render_template('indexnew.html', Tasks = tasks)
     
-    @app.route('/compelte_day', methods=['POST'])
+    @app.route('/complete_day', methods=['POST'])
     def complete_day():
-        current_day = request.form.get('current_day', type=int)
         tasks = todo.query.all()
         for task in tasks:
-             task.day += 1
-
+            try:
+                old_date = datetime.strptime(task.day, "%d/%m")
+                new_date = old_date + timedelta(days=1)
+                task.day = new_date.strftime("%d/%m")
+            except ValueError:
+                continue 
         db.session.commit()
         flash("Day Completed")
         return redirect(url_for('tasks'))
 
     @app.route('/tasks', methods=['GET'])
     def tasks():
-        tasks = todo.query.all()
-        return render_template('todo.html', Tasks = tasks, types = types)
 
+        tasks = todo.query.order_by(todo.day, todo.value).all()
+        today = date.today()
+        today_str = today.strftime("%d/%m")
+        yesterday = today - timedelta(days=1)
+        yesterday_str = yesterday.strftime("%d/%m")
+        today_tasks = [t for t in tasks if t.day == today_str]
+        yesterday_tasks = [t for t in tasks if t.day == yesterday_str]
+        existing_count = todo.query.filter_by(day=today_str).count()
+        next_value = existing_count + 1
+
+        return render_template(
+            'todo.html',
+            today_tasks=today_tasks,
+            yesterday_tasks=yesterday_tasks,
+            types=types,
+            next_value=next_value,
+            next_day_value=today_str,
+            current_date=today_str,
+            weekday=today.strftime("%A")
+        )
+    
     @app.route('/add', methods=['POST'])
     def add():
-            name = request.form['name']
-            type_ = request.form['type']
-            day = int(request.form['day'])
-            existing_count = todo.query.filter_by(day=day).count()
+        name = request.form.get('name')
+        type_ = request.form.get('type')
+        day = request.form.get('day')  # now a string like "10/09"
+        existing_count = todo.query.filter_by(day=day).count()
 
-            add_task = todo(
-                name=request.form['name'],
-                type=request.form['type'],
-                day=int(request.form['day']),
-                value=existing_count + 1
-            )
-            db.session.add(add_task)
-            db.session.commit()
-            flash("Task Added Successfully")
-            return redirect(url_for('tasks'))  
+        add_task = todo(
+            name=name,
+            type=type_,
+            day=day,
+            value=existing_count + 1
+        )
+        db.session.add(add_task)
+        db.session.commit()
+        flash("Task Added Successfully")
+        return redirect(url_for('tasks'))
 
     @app.route('/edit', methods=['POST'])
     def edit():
         task_id = request.form.get('id')
         task = todo.query.get_or_404(task_id)
-        
+
+        old_value = task.value
+        old_day = task.day
+
         task.name = request.form['name']
         task.type = request.form['type']
-        task.day = int(request.form['day'])
-        task.value = float(request.form['value'])
+        task.day = (request.form['day'])
+        new_value = int(request.form['value'])
+
+        if task.day == old_day:
+            if new_value < old_value:
+                tasks_to_shift = todo.query.filter(
+                    todo.day == task.day,
+                    todo.value >= new_value,
+                    todo.value < old_value,
+                    todo.id != task.id
+                ).all()
+                for t in tasks_to_shift:
+                    t.value += 1
+            elif new_value > old_value:
+                tasks_to_shift = todo.query.filter(
+                    todo.day == task.day,
+                    todo.value <= new_value,
+                    todo.value > old_value,
+                    todo.id != task.id
+                ).all()
+                for t in tasks_to_shift:
+                    t.value -= 1
+        else:
+            tasks_to_shift = todo.query.filter(
+                todo.day == task.day,
+                todo.value >= new_value
+            ).all()                                                                                                                                                                     
+            for t in tasks_to_shift:
+                t.value += 1
+
+        task.value = new_value
 
         db.session.commit()
-        flash("Task Successfully Edited")
-        return redirect(url_for('tasks'))
-
-    @app.route('/delete', methods=['GET'])
+        flash("Task Successfully Edited & Reordered")
+        return redirect(url_for('tasks'))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+    
+    @app.route('/delete', methods=['GET'])  
     def delete():
-        id = request.args.get('id')  	# Get the item ID from form
-        tasks = todo.query.get(id)  # Fetch item by ID
-        db.session.delete(todo)  	# Delete item
-        db.session.commit()  		# Commit changes
-        return redirect(url_for('home'))
+        task_id = request.args.get('id')
+        task = todo.query.get_or_404(task_id)
+        db.session.delete(task)
+        db.session.commit()
+        flash("Task Deleted Successfully")
+        return redirect(url_for('tasks'))  
+   
+    @app.route('/affirmations', methods=['GET'])
+    def affirmations():
+        current_date = get_current_day()
+        current_weekday = get_current_weekday()
+        affirmations_list = Affirmation.query.all()
+        return render_template(
+            'affirmations.html',
+            Affirmations=affirmations_list,
+            current_date=current_date,
+            current_weekday=current_weekday
+        )
+
+    @app.route('/add_affirmation', methods=['POST'])
+    def add_affirmation():
+        text = request.form.get('text')
+        day = get_current_day()  # automatically today's date
+        if text:
+            new_affirmation = Affirmation(text=text, day=day)
+            db.session.add(new_affirmation)
+            db.session.commit()
+            flash("Affirmation Added Successfully")
+        return redirect(url_for('affirmations'))
+
+    @app.route('/edit_affirmation', methods=['POST'])
+    def edit_affirmation():
+        aff_id = request.form.get('id')
+        affirmation = Affirmation.query.get_or_404(aff_id)
+        affirmation.text = request.form.get('text')
+        db.session.commit()
+        flash("Affirmation Edited Successfully")
+        return redirect(url_for('affirmations'))
+    
+    @app.route('/toggle_task/<int:task_id>', methods=['POST'])
+    def toggle_task(task_id):
+        task = todo.query.get_or_404(task_id)
+        completed = request.json.get('completed', False)
+        task.completed = completed
+        db.session.commit()
+        return {"success": True}
+    
+def get_current_day():
+    today = date.today()
+    return today.strftime("%d/%m/%y")  # e.g., "10/09/25"
+
+def get_current_weekday():
+    today = date.today()
+    return today.strftime("%A")  # e.g., "Monday"
